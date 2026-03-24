480b8c2aff1ec04defe7814a1690d19b7408b5cc
Егор <darkman8350@gmail.com>
2026-03-24 23:36:04 +0300

    config fix

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,11 @@
 import os
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
+    model_config = SettingsConfigDict(extra='ignore', env_file='.env')
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,44 +1,54 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.api.v1 import endpoints
 from app.db.database import engine, Base, AsyncSessionLocal
 from app.db.models import User
 from app.core.security import get_password_hash, generate_random_password
-from app.core.config import settings
 from sqlalchemy import select
-app = FastAPI(title="Tuwunel Monitor")
-# Подключаем роуты
-app.include_router(endpoints.router, prefix="/api/v1")
 
 
-@app.on_event("startup")
-async def startup_event():
-    # 1. Создаем таблицы
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # --- Логика Запуска (Startup) ---
+
+    # Создаем таблицы
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
     DEFAULT_ADMIN_USERNAME = "admin"
-    # 2. Проверяем и создаем/обновляем админа
+
+    # Проверяем и создаем админа
     async with AsyncSessionLocal() as session:
         result = await session.execute(select(User).limit(1))
         user = result.scalar_one_or_none()
 
         if not user:
             password = generate_random_password()
-            print(f"Creating default admin user '{settings.ADMIN_USERNAME}'...")
+            print(f"Creating default admin user '{DEFAULT_ADMIN_USERNAME}'...")
+
             new_user = User(
                 username=DEFAULT_ADMIN_USERNAME,
                 hashed_password=get_password_hash(password),
                 role="admin"
             )
             session.add(new_user)
-            # Удобный вывод в консоль
+
             print("\n" + "=" * 40)
             print(f" Login:    {DEFAULT_ADMIN_USERNAME}")
             print(f" Password: {password}")
             print("=" * 40 + "\n")
+
         await session.commit()
+
+    yield
+
+app = FastAPI(title="Tuwunel Monitor", lifespan=lifespan)
+
+# Подключаем роуты
+app.include_router(endpoints.router, prefix="/api/v1")
 
 
 @app.get("/")
 def read_root():
     return {"status": "Server Monitor is running"}
-
